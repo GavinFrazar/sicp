92b68a07288947e7228447d32df91e4f0ed5962a
Gavin Frazar <gavinfrazar@gmail.com>
2020-12-09 20:36:17 -0800

    Add ch2 work

--- ch2.rkt
+++ ch2.rkt
@@ -1,5 +1,7 @@
 #lang racket
 (require "ch1.rkt")
+(require sicp-pict)
+
 (define (average a b)
   (/ (+ a b) 2))
 (define (square x)
@@ -339,25 +341,19 @@
       (let ((rest (subsets (cdr s))))
         (append rest (map (lambda (head) (cons (car s) head)) rest)))))
 
-;; bugged?
-;; (define (accumulate op identity-elem sequence)
-;;   (define (iter remaining acc)
-;;     (cond ((null? remaining) acc)
-;;           ((pair? remaining) (iter (cdr remaining)
-;;                                    (op (car remaining) acc)))
-;;           (else (op remaining acc))))
-;;   (reverse (iter sequence identity-elem)))
 (define (accumulate op initial sequence)
-  (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
+  (cond [(null? sequence) initial]
+        [(pair? sequence) (op (car sequence)
+                              (accumulate op initial (cdr sequence)))]))
 
 ;; Exercise 2.33
-(define (map p sequence)
-  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+;; (define (map p sequence)
+;;   (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
 (define (append seq1 seq2)
-  (accumulate cons seq2 seq1))
+  (if (or (pair? seq2) (null? seq2))
+      (accumulate cons seq2 seq1)
+      (accumulate cons (list seq2) seq1)))
+
 (define (length sequence)
   (accumulate (lambda (x y) (inc y)) 0 sequence))
 
@@ -378,3 +374,371 @@
       nil
       (cons (accumulate op init (map car seqs))
             (accumulate-n op init (map cdr seqs)))))
+
+;; Exercise 2.37
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (λ (row) (dot-product row v))
+       m))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (λ (row) (matrix-*-vector cols row)) m)))
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(define (fold-right op initial sequence)
+  (cond [(null? sequence) initial]
+        [else (op (car sequence) (fold-right op initial (cdr sequence)))]))
+
+(define (reverse-v2 sequence)
+  (define (push value sequence)
+    (fold-right cons (list value) sequence))
+  (fold-right (λ (elem acc) (push elem acc)) '() sequence))
+
+(define (reverse-v3 sequence)
+  (fold-left (λ (acc elem) (cons elem acc)) nil sequence))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (flatten seq)
+  (flatmap (λ (x) x) seq))
+
+;; Exercise 2.40
+(define (unique-pairs n)
+  (flatmap (λ (i) {map (λ (j) {list j i}) (range 1 i)})
+       (range 2 (+ n 1))))
+
+(define (unique-triples n)
+  (flatmap (λ (k) {map (λ (pair) {append pair (list k)})
+                       (unique-pairs (- k 1))})
+       (range 3 (+ n 1))))
+
+;; Exericse 2.41 -- not quite 3Sum is it
+(define (sum-pair pair)
+  (+ (car pair) (cadr pair)))
+
+(define (two-sum goal-sum)
+  {filter (λ (pair) {= (sum-pair pair) goal-sum})
+          (unique-pairs goal-sum)})
+
+(define (sum-triple triple)
+  (+ (car triple) (sum-pair (cdr triple))))
+
+(define (three-sum n goal-sum)
+  (filter (λ (triple) {= goal-sum (sum-triple triple)})
+          (unique-triples n)))
+
+;; Generalized for my own amusement:
+(define (unique-k-tuples k n)
+  (cond [(= k 2) (unique-pairs n)]
+        [else (flatmap (λ (x) {map (λ (j-tuple) {append j-tuple (list x)})
+                                   (unique-k-tuples (- k 1) (- x 1))})
+                       (range k (+ n 1)))]))
+
+(define (sum-k-tuple k k-tuple)
+  (cond [(= k 2) (sum-pair k-tuple)]
+        [else (+ (car k-tuple)
+                 (sum-k-tuple (- k 1) (cdr k-tuple)))]))
+
+(define (k-sum k n goal-sum)
+  (filter (λ (k-tuple) {= goal-sum (sum-k-tuple k k-tuple)})
+          (unique-k-tuples k n)))
+
+;; Exercise 2.42 -- TODO fix this solution idk what is wrong with it
+(define (make-queen row col)
+  (cons row col))
+
+(define (get-row queen)
+  (car queen))
+
+(define (get-col queen)
+  (cdr queen))
+
+(define (diff-queen a b)
+  (list (abs (- (car a) (car b)))
+        (abs (- (cadr a) (cadr b)))))
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cond [(null? rest-of-queens) (list (make-queen new-row k))]
+        [else (cons (make-queen new-row k) rest-of-queens)]))
+
+(define empty-board '())
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (!= a b)
+  (not (= a b)))
+
+(define (safe? k positions)
+  (define (safe-one? a b)
+    (cond [(null? b) #t]
+          [else ((let ((diff (diff-queen a b)))
+                   (and (!= (get-row a) (get-row b))
+                        (and (> (get-row diff) 0)
+                             (> (get-col diff) 0)))))]))
+  (filter (λ (queens)
+            (displayln positions)
+            {fold-left (λ (acc queen)
+                         {and acc (safe-one? (car queens) queen)})
+                       #t (cdr queens)})
+          positions))
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+          (flatmap
+          (lambda (rest-of-queens)
+            (displayln rest-of-queens)
+            (map (lambda (new-row)
+                    (adjoin-position new-row k rest-of-queens))
+                  (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+;; Derivatives
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond [(=number? a1 0) a2]
+        [(=number? a2 0) a1]
+        [(and (number? a1) (number? a2)) (+ a1 a2)]
+        [else (list '+ a1 a2)]))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s)
+  (cadr s))
+
+(define (augend s)
+  (if (null? (cdddr s))
+      (caddr s)
+      (cons '+ (cddr s))))
+
+(define (make-product m1 m2 . rest-terms)
+  (cond [(or (=number? m1 0) (=number? m2 0)) 0]
+        [(=number? m1 1) m2]
+        [(=number? m2 1) m1]
+        [(and (number? m1) (number? m2)) (* m1 m2)]
+        [else (list '* m1 m2)]))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier p)
+  (cadr p))
+
+(define (multiplicand p)
+  (if (null? (cdddr p))
+      (caddr p)
+      (cons '* (cddr p))))
+
+(define (make-exponentiation b e)
+  (cond [(=number? b 0) 0]
+        [(=number? e 0) 1]
+        [(=number? e 1) b]
+        [else (list '** b e)]))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+
+(define (base e)
+  (cadr e))
+
+(define (exponent e)
+  (caddr e))
+
+(define (deriv exp var)
+  (cond [(number? exp) 0]
+        [(variable? exp)
+          (if (same-variable? exp var) 1 0)]
+        [(sum? exp)
+          (make-sum (deriv (addend exp) var)
+                    (deriv (augend exp) var))]
+        [(product? exp)
+          (make-sum
+            (make-product (multiplier exp)
+                          (deriv (multiplicand exp) var))
+            (make-product (deriv (multiplier exp) var)
+                          (multiplicand exp)))]
+        [(exponentiation? exp)
+          (make-product (exponent exp)
+                        (make-exponentiation (base exp)
+                                              (make-sum (exponent exp) '-1)))]
+        [else
+          (error "unknown expression type -- DERIV" exp)]))
+
+(define (parse-exp exp)
+  (define (parse-right exp result)
+    (cond [(null? exp) result]
+          [(eq? (car exp) '+) (make-sum result (parse-exp (cdr exp)))]
+          [(eq? (car exp) '*)
+           (parse-right (cddr exp)
+                        (make-product result (parse-exp (cadr exp))))]
+          [else (displayln "ERROR -- parse-right")]))
+
+  (cond [(number? exp) exp]
+        [(variable? exp) exp]
+        [(pair? exp)
+         (if (null? (cdr exp))
+             (parse-exp (car exp))
+             (parse-right (cdr exp) (parse-exp (car exp))))]
+        [else (displayln "ERROR -- parse-exp")]))
+;; (x + 3 * y + 4)
+;;
+
+;; Sets
+
+;; For my sanity
+(define false #f)
+(define true #t)
+
+;; (define (element-of-set? x set)
+;;   (cond ((null? set) false)
+;;         ((= x (car set)) true)
+;;         ((< x (car set)) false)
+;;         (else (element-of-set? x (cdr set)))))
+
+;; Exercise 2.61
+;; (define (adjoin-set x set)
+;;   (define (insert-in-order x set)
+;;     (cond [(null? set) (list x)]
+;;           [(< x (car set)) (cons x set)]
+;;           [else (cons (car set)
+;;                       (insert-in-order x (cdr set)))]))
+
+;;   (if (element-of-set? x set)
+;;       set
+;;       (insert-in-order x set)))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond ((= x1 x2)
+              (cons x1
+                    (intersection-set (cdr set1)
+                                      (cdr set2))))
+              ((< x1 x2)
+              (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+              (intersection-set set1 (cdr set2)))))))
+
+;; Exercise 2.59
+;; (define (union-set set1 set2)
+;;   (cond [(null? set1) set2]
+;;         [(null? set2) set1]
+;;         [(element-of-set? (car set1) set2)
+;;          (union-set (cdr set1) set2)]
+;;         [else (cons (car set1)
+;;                     (union-set (cdr set1) set2))]))
+
+;; Exercise 2.62
+(define (union-set set1 set2)
+  (cond [(null? set1) set2]
+        [(null? set2) set1]
+        [else (let ((x1 (car set1))
+                    (x2 (car set2)))
+                (cond [(= x1 x2)
+                       (cons x1
+                             (union-set (cdr set1)
+                                        (cdr set2)))]
+                      [(< x1 x2)
+                       (cons x1
+                             (union-set (cdr set1)
+                                        set2))]
+                      [(< x2 x1)
+                       (cons x2
+                             (union-set set1 (cdr set2)))]))]))
+
+(define (entry tree) (car tree))
+
+(define (left-child tree) (cadr tree))
+
+(define (right-child tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+        (element-of-set? x (left-child set)))
+        ((> x (entry set))
+        (element-of-set? x (right-child set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+        (make-tree (entry set)
+                    (adjoin-set x (left-child set))
+                    (right-child set)))
+        ((> x (entry set))
+        (make-tree (entry set)
+                    (left-child set)
+                    (adjoin-set x (right-child set))))))
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-child tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-child tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-child tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-child tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
