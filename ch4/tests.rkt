2169a0d506d861a985357c042e0cc42fb5fb600e
Gavin Frazar <gavinfrazar@gmail.com>
2020-12-15 23:58:25 -0800

    Fix alignment

--- ch4/tests.rkt
+++ ch4/tests.rkt
@@ -35,7 +35,7 @@
   (syntax-rules (=>)
     [(_ tester suite-name (test-case-name ... test-expr => test-expected) ...)
      (test-suite suite-name
-       (tester test-case-name ... test-expr => test-expected) ...)]))
+                 (tester test-case-name ... test-expr => test-expected) ...)]))
 
 (define-syntax eval-test-suite
   (syntax-rules ()
@@ -52,30 +52,32 @@
       (1 => 1)
       ("abc" => "abc"))
 
-    (eval-test-suite "primitive-procedure"
-      ((cons 1 2) => (cons 1 2))
-      ((car (cons 1 2)) => 1)
-      ((cdr (cons 1 2)) => 2)
-      ((+ 1 1) => 2)
-      ((- 1 1) => 0)
-      ((* 2 3) => 6)
-      ((= 1 1) => #t)
-      ((= 1 2) => #f)
-      ((eq? (cons 1 2) (cons 1 2)) => #f)
-      ((eq? 1 1) => #t))
+    (eval-test-suite
+     "primitive-procedure"
+     ((cons 1 2) => (cons 1 2))
+     ((car (cons 1 2)) => 1)
+     ((cdr (cons 1 2)) => 2)
+     ((+ 1 1) => 2)
+     ((- 1 1) => 0)
+     ((* 2 3) => 6)
+     ((= 1 1) => #t)
+     ((= 1 2) => #f)
+     ((eq? (cons 1 2) (cons 1 2)) => #f)
+     ((eq? 1 1) => #t))
 
-    (eval-test-suite "begin"
-      ((begin 1) => 1)
-      ("begin form evals to last expression's value"
-       (begin 1 2 3) => 3)
-      ("begin/complex 1"
-       (begin
-         (define (fact n)
-           (if (= n 0)
-               1
-               (* n (fact (- n 1)))))
-         (fact 10))
-       => 3628800)))
+    (eval-test-suite
+     "begin"
+     ((begin 1) => 1)
+     ("begin form evals to last expression's value"
+      (begin 1 2 3) => 3)
+     ("begin/complex 1"
+      (begin
+        (define (fact n)
+          (if (= n 0)
+              1
+              (* n (fact (- n 1)))))
+        (fact 10))
+      => 3628800)))
   (run-tests all-eval-tests))
 
 ;;(test/eval core:eval core:the-global-environment) ;; test my testing :D
