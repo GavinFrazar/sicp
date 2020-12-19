a13f8b22fafc07c04af4be2a11974894c768b027
Gavin Frazar <gavinfrazar@gmail.com>
2020-12-19 14:47:11 -0800

    Solve exercise 4.4

---
+++ ch4/4-4.rkt
@@ -0,0 +1,46 @@
+#lang sicp
+
+(#%require "ddcore.rkt"
+           "tests.rkt")
+
+(define (eval-and exp env)
+  (define (and-eval-loop exps last-val)
+    (if (null? exps)
+        last-val
+        (let ([next-val (eval (car exps) env)])
+          (if (true? next-val)
+              (and-eval-loop (cdr exps) next-val)
+              'false))))
+  (and-eval-loop (and-operands exp) 'true))
+
+(define (and-operands exp)
+  (cdr exp))
+
+(define (eval-or exp env)
+  (define (or-eval-loop exps)
+    (if (null? exps)
+        'false
+        (let ([next-val (eval (car exps) env)])
+          (if (true? next-val)
+              next-val
+              (or-eval-loop (cdr exps))))))
+  (or-eval-loop (or-operands exp)))
+
+(define (or-operands exp)
+  (cdr exp))
+
+
+(install-special-form `(and ,eval-and))
+(install-special-form `(or ,eval-or))
+
+(test-eval (and) => 'true)
+(test-eval (and 1) => 1)
+(test-eval (and false) => 'false)
+(test-eval (and 1 false 2) => 'false)
+
+(test-eval (or) => 'false)
+(test-eval (or 1) => 1)
+(test-eval (or 1 2) => 1)
+(test-eval (or false 1) => 1)
+(test-eval (or 1 false) => 1)
+
