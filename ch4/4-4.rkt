7e5fe0031ebe7d06358a00ed9e8a4fa728e84ebf
Gavin Frazar <gavinfrazar@gmail.com>
2020-12-20 02:34:51 -0800

    Convert tests into test suite, provide eval forms

--- ch4/4-4.rkt
+++ ch4/4-4.rkt
@@ -3,6 +3,9 @@
 (#%require "ddcore.rkt"
            "tests.rkt")
 
+(#%provide eval-and
+           eval-or)
+
 (define (eval-and exp env)
   (define (and-eval-loop exps last-val)
     (if (null? exps)
@@ -29,19 +32,25 @@
 (define (or-operands exp)
   (cdr exp))
 
-
 (install-special-form `(and ,eval-and))
 (install-special-form `(or ,eval-or))
 
-(test-eval (and) => 'true)
-(test-eval (and 1) => 1)
-(test-eval (and 1 2) => 2)
-(test-eval (and false) => 'false)
-(test-eval (and 1 false 2) => 'false)
-
-(test-eval (or) => 'false)
-(test-eval (or 1) => 1)
-(test-eval (or 1 2) => 1)
-(test-eval (or false 1) => 1)
-(test-eval (or 1 false) => 1)
+(define-eval-test-suite
+  and-or-tests
+  ("and w/o values returns false"
+   (and) => true)
+  ("and with a single truthy value returns its value"
+   (and 1) => 1)
+  ("and with all truthy values returns the last value"
+   (and 1 2) => 2)
+  ("and with a false arg returns false"
+   (and false) => false)
+  ((and 1 false 2) => false)
+  ((or) => false)
+  ((or 1) => 1)
+  ((or 1 2) => 1)
+  ((or false 1) => 1)
+  ((or 1 false) => 1)
+  ("nested predicate evaluates correctly"
+   (or (and false) 42) => 42))
 
