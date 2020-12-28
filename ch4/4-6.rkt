381a7eaf66655e3819178217d9585187e87b6d2a
Gavin Frazar <gavinfrazar@gmail.com>
2020-12-27 23:09:38 -0800

    Solve exercise 4.6

---
+++ ch4/4-6.rkt
@@ -0,0 +1,41 @@
+#lang sicp
+
+(#%require "ddcore.rkt"
+           (only racket/base
+                 provide prefix-out module+)
+           rackunit)
+(provide (prefix-out 4-6: let->combination))
+
+(define (let->combination exp)
+  (let ([bindings (let-bindings exp)])
+    (apply list
+           (make-lambda (binding-parameters bindings) (list (let-body exp)))
+           (binding-expressions bindings))))
+
+(define (let-bindings exp)
+  (cadr exp))
+
+(define (let-body exp)
+  (caddr exp))
+
+(define (binding-parameters bindings)
+  (map car bindings))
+
+(define (binding-expressions bindings)
+  (map cadr bindings))
+
+(module+ test
+  (define bindings '((a (+ x 1))
+                     (b 42)))
+  (define body '(+ a b))
+  (define test-exp `(let ,bindings ,body))
+  (check-equal? (let-bindings test-exp)
+                bindings)
+  (check-equal? (let-body test-exp)
+                body)
+  (check-equal? (binding-parameters bindings)
+                '(a b))
+  (check-equal? (binding-expressions bindings)
+                '((+ x 1) 42))
+  (check-equal? (let->combination test-exp)
+                '((lambda (a b) (+ a b)) (+ x 1) 42)))
